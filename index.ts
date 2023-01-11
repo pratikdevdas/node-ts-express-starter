df22c686c6f672e4b7021dbdd0f8aca2480046f4
Revenant <63319865+pratikdevdas@users.noreply.github.com>
2023-01-11 12:22:10 +0530

    updated with prettier

--- index.ts
+++ index.ts
@@ -5,10 +5,10 @@ app.use(express.json());
 const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+    console.log('someone pinged here');
+    res.send('pong');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
